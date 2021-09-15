8df0aabb308535efd024d79e2497f5b85c47e34b
23felons23 <slavikzhyhota@gmail.com>
2021-09-15 21:09:22 +0200

    add basic project structure + add theme

---
+++ lib/config/themes/dark_theme.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+
+  static const Color background = Color(0xFF0F0F12);
+  static const Color backgroundVariant = Color(0xFF1A1A28);
+
+  static const Color primary = Color(0xFF7172e3);
+  static const Color green = Color(0xFF3DE488);
+  static const Color epic = Color(0xFFDE54EB);
+  static const Color orange = Color(0xFFFFA946);
+
+
+  static const Color text = Color(0xFFFDFDFD);
+
+  static const Color text95 = Color(0xFFFCFCFC);  // 95% opacity
+  static const Color text90 = Color(0xFFFBFBFB);  // 90% opacity
+  static const Color text80 = Color(0xFFF8F8F8);  // 80% opacity
+
+  const AppColors();
+}
+
+class AppTheme {
+
+  static const colors = AppColors();
+
+  AppTheme._();
+
+  static final theme = ThemeData(
+      fontFamily: "Bebas Neue",
+
+      textTheme: const TextTheme(
+        headline1: TextStyle(color: AppColors.text, fontSize: 38)
+      )
+  );
+}
