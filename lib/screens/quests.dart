51ae8a1fcf74dc4b8b49650e96c76edc7c01be99
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-09-20 21:15:18 +0200

    added http package

--- lib/screens/quests.dart
+++ lib/screens/quests.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:winhalla_app/widgets/navigation_bar.dart';
 import 'package:winhalla_app/screens/quests.dart';
 import 'package:winhalla_app/config/themes/dark_theme.dart';
 
 class Quests extends StatelessWidget {
-
   const Quests({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: AppColors.background,
-        body: Text("test"));
+        body: FutureBuilder(
+          future: http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')),
+          builder: (dynamic context, dynamic res) {
+            return Text("test");
+          },
+        ));
   }
 }
