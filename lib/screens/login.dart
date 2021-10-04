fe1493e42944b067557d2a8ee907d4b6cb8dc363
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-10-04 21:50:56 +0200

    fixed bug on login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -49,11 +49,9 @@ class _LoginPageState extends State<LoginPage> {
             child: Scaffold(
                 backgroundColor: kBackground,
                 body: ChangeNotifierProvider<LoginPageManager>(
-                    create: (_) => LoginPageManager(step.data == "no data"
-                        ? 0
-                        : int.parse(step.data as String)),
-                    child:
-                        Consumer<LoginPageManager>(builder: (context, page, _) {
+                    create: (_) =>
+                        LoginPageManager(step.data == "no data" ? 0 : int.parse(step.data as String)),
+                    child: Consumer<LoginPageManager>(builder: (context, page, _) {
                       return screenList[page.page == "no data" ? 0 : page.page];
                     })) // Can't be null but the compiler doesn't sees it so bidouillage
                 ),
@@ -72,80 +70,77 @@ class WinhallaAccountCreation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      future: getNonNullSSData("tempSteamId"),
-      builder: (context,AsyncSnapshot<String> key) {
-        if(!key.hasData) return Container();
-        bidTextController.text = key.data == null?"":key.data as String;
-        return Column(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border:
-                      Border.all(width: 1, color: kText, style: BorderStyle.solid),
-                  color: kText),
-              child: TextField(
-                controller: bidTextController,
-                style: TextStyle(fontSize: 20),
-                decoration: InputDecoration(
-                  hintText: 'Brawlhalla ID',
-                  contentPadding: EdgeInsets.all(15),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border:
-                      Border.all(width: 1, color: kText, style: BorderStyle.solid),
-                  color: kText),
-              child: TextField(
-                controller: linkTextController,
-                style: TextStyle(fontSize: 20),
-                decoration: InputDecoration(
-                  hintText: 'Link id',
-                  contentPadding: EdgeInsets.all(15),
-                  border: InputBorder.none,
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            GestureDetector(
-              child: Container(
-                padding: EdgeInsets.fromLTRB(19, 9, 19, 6),
-                child: Text(
-                  'Create winhalla account',
-                  style: kHeadline1.apply(color: kRed),
-                ),
+        future: getNonNullSSData("tempSteamId"),
+        builder: (context, AsyncSnapshot<String> key) {
+          if (!key.hasData) return Container();
+          bidTextController.text = key.data == null ? "" : key.data as String;
+          return Column(
+            children: [
+              Container(
                 decoration: BoxDecoration(
-                  color: kBackgroundVariant,
-                  borderRadius: BorderRadius.circular(20),
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(width: 1, color: kText, style: BorderStyle.solid),
+                    color: kText),
+                child: TextField(
+                  controller: bidTextController,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    hintText: 'Brawlhalla ID',
+                    contentPadding: EdgeInsets.all(15),
+                    border: InputBorder.none,
+                  ),
                 ),
               ),
-              onTap: () async {
-                final String? secureStorageKey =
-                    await secureStorage.read(key: "authKey");
-                if (secureStorageKey == null) return;
-                print(secureStorageKey);
-                var linkId = await http.post(
-                  getUri(
-                      "/auth/createAccount?linkId=${linkTextController.text}&BID=${bidTextController.text}"),
-                  headers: {"authorization": secureStorageKey},
-                );
-                print(linkId.body);
-              },
-            )
-          ],
-          crossAxisAlignment: CrossAxisAlignment.start,
-        );
-      }
-    );
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    border: Border.all(width: 1, color: kText, style: BorderStyle.solid),
+                    color: kText),
+                child: TextField(
+                  controller: linkTextController,
+                  style: TextStyle(fontSize: 20),
+                  decoration: InputDecoration(
+                    hintText: 'Link id',
+                    contentPadding: EdgeInsets.all(15),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              GestureDetector(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(19, 9, 19, 6),
+                  child: Text(
+                    'Create winhalla account',
+                    style: kHeadline1.apply(color: kRed),
+                  ),
+                  decoration: BoxDecoration(
+                    color: kBackgroundVariant,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                onTap: () async {
+                  final String? secureStorageKey = await secureStorage.read(key: "authKey");
+                  if (secureStorageKey == null) return;
+
+                  var linkId = await http.post(
+                    getUri(
+                        "/auth/createAccount?linkId=${linkTextController.text}&BID=${bidTextController.text}"),
+                    headers: {"authorization": secureStorageKey},
+                  );
+                  print(linkId.body);
+                  Navigator.pushReplacementNamed(context, "/");
+                },
+              )
+            ],
+            crossAxisAlignment: CrossAxisAlignment.start,
+          );
+        });
   }
 }
 
@@ -156,62 +151,62 @@ class GoogleAppleLogin extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(30,50,30,0),
+        padding: const EdgeInsets.fromLTRB(30, 50, 30, 0),
         child: Column(
           children: [
             SizedBox(
               width: 300,
-              child: Consumer<LoginPageManager>(
-                builder: (context, page,_) {
-                  return GestureDetector(
-                    child: Container(
-                      padding: const EdgeInsets.fromLTRB(19, 9, 19, 6),
-                      child: Text(
-                        'Login With google',
-                        style: kHeadline1.apply(color: kRed),
-                      ),
-                      decoration: BoxDecoration(
-                        color: kBackgroundVariant,
-                        borderRadius: BorderRadius.circular(20),
-                      ),
+              child: Consumer<LoginPageManager>(builder: (context, page, _) {
+                return GestureDetector(
+                  child: Container(
+                    padding: const EdgeInsets.fromLTRB(19, 9, 19, 6),
+                    child: Text(
+                      'Login With google',
+                      style: kHeadline1.apply(color: kRed),
                     ),
-                    onTap: () async {
-                      var temp = await GoogleSignInApi.login();
-                      if (temp?["auth"].accessToken == null) return;
-                      dynamic idToken;
-                      try {
-                        idToken = await http.post(getUri("/auth/createToken"), body: {
-                          "token": temp?["auth"].accessToken,
-                          "name": temp?['account'].displayName,
-                          if (temp?['account'].photoUrl != null)
-                            "picture": temp?['account'].photoUrl
-                        });
-                      } catch (e) {
-                        print(e);
-                      }
-                      secureStorage.write(
-                          key: "authKey", value: jsonDecode(idToken.body)["_id"]);
-                      page.next();
-                    },
-                  );
-                }
-              ),
+                    decoration: BoxDecoration(
+                      color: kBackgroundVariant,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                  ),
+                  onTap: () async {
+                    var temp = await GoogleSignInApi.login();
+                    if (temp?["auth"].accessToken == null) return;
+                    dynamic idToken;
+                    try {
+                      idToken = await http.post(getUri("/auth/createToken"), body: {
+                        "token": temp?["auth"].accessToken,
+                        "name": temp?['account'].displayName,
+                        if (temp?['account'].photoUrl != null) "picture": temp?['account'].photoUrl
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
+                    await secureStorage.write(key: "authKey", value: jsonDecode(idToken.body)["_id"]);
+                    page.next();
+                  },
+                );
+              }),
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             SizedBox(
               width: 300,
               child: Container(
-                child: GestureDetector(child:Container(
-                  padding: EdgeInsets.fromLTRB(19, 9, 19, 6),
-                  child: Text(
-                    'Login With Apple',
-                    style: kHeadline1.apply(color: kRed),
+                child: GestureDetector(
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(19, 9, 19, 6),
+                    child: Text(
+                      'Login With Apple',
+                      style: kHeadline1.apply(color: kRed),
+                    ),
+                    decoration: BoxDecoration(
+                      color: kBackgroundVariant,
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
-                  decoration: BoxDecoration(
-                    color: kBackgroundVariant,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),),
+                ),
               ),
             )
             /*GestureDetector(
@@ -237,7 +232,6 @@ class GoogleAppleLogin extends StatelessWidget {
   }
 }
 
-
 class WinhallaPresentation extends StatelessWidget {
   const WinhallaPresentation({Key? key}) : super(key: key);
 
@@ -245,7 +239,7 @@ class WinhallaPresentation extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(30,50,30,0),
+        padding: const EdgeInsets.fromLTRB(30, 50, 30, 0),
         child: Column(
           children: [
             Text(
@@ -255,7 +249,13 @@ class WinhallaPresentation extends StatelessWidget {
             Consumer<LoginPageManager>(
               builder: (context, page, _) => TextButton(
                 onPressed: () => page.next(),
-                child: Text("Go to next page",style: kBodyText2.apply(fontFamily: "Bebas neue",color: kPrimary,),),
+                child: Text(
+                  "Go to next page",
+                  style: kBodyText2.apply(
+                    fontFamily: "Bebas neue",
+                    color: kPrimary,
+                  ),
+                ),
               ),
             ),
           ],
