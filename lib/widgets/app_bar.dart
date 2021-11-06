a4ca529e3ed0dd5474a12074675feebc481ecf9f
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-11-06 16:57:59 +0100

    added account panel on click on user icon

--- lib/widgets/app_bar.dart
+++ lib/widgets/app_bar.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:provider/provider.dart';
 import 'package:winhalla_app/config/themes/dark_theme.dart';
+import 'package:winhalla_app/utils/services/secure_storage_service.dart';
 import 'package:winhalla_app/utils/user_class.dart';
 import 'package:winhalla_app/widgets/popup_legal.dart';
 import 'package:winhalla_app/widgets/popup_link.dart';
@@ -32,7 +33,104 @@ class MyAppBar extends StatelessWidget {
                 child: GestureDetector(
                   onTap: () {
                     var linkId = context.read<User>().value["user"]["linkId"];
-                    showDialog(context: context, builder: (BuildContext context)=>LinkInfoWidget(linkId));
+                    late OverlayEntry overlayEntry;
+                    overlayEntry = OverlayEntry(
+                      builder: (context) {
+                        return DefaultTextStyle(
+                          style: const TextStyle(fontFamily: "Bebas neue"),
+                          child: Stack(
+                            children: [
+                              Positioned.fill(
+                                  child: GestureDetector(
+                                    onTapDown:(_){
+                                      overlayEntry.remove();
+                                    },
+                                    child: Container(
+                                      color: Colors.transparent,
+                                    ),
+                                  )
+                              ),
+                              Positioned(
+                                top:125,
+                                right:20,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: kBackgroundVariant,
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow:[
+                                      BoxShadow(
+                                        color: Colors.black.withOpacity(0.8), //color of shadow
+                                        spreadRadius: 5, //spread radius
+                                        blurRadius: 8, // blur radius
+                                      ),
+                                    ],
+                                  ),
+                                  child:Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      GestureDetector(
+                                        behavior:HitTestBehavior.translucent,
+                                        onTap:() async {
+                                          await secureStorage.write(key: "authKey", value: null);
+                                          Navigator.pop(context);
+                                          await Future.delayed(const Duration(milliseconds: 150));
+                                          Navigator.pushNamed(context, "/login");
+                                          overlayEntry.remove();
+                                        },
+                                        child: Container(
+                                          margin: const EdgeInsets.fromLTRB(24,19,24,0),
+                                          child: Row(
+                                            children: [
+                                              const Icon(Icons.logout,color:kRed,size: 30,),
+                                              const SizedBox(width: 10,),
+                                              Text("Logout",style: kBodyText2.apply(fontFamily: "Bebas Neue"),),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        margin: const EdgeInsets.fromLTRB(24,15,24,15),
+                                        child: Row(
+                                          children: [
+                                            const Icon(Icons.add_circle_outline,color:kPrimary,size: 30,),
+                                            const SizedBox(width: 10,),
+                                            Text("Add Account",style: kBodyText2.apply(fontFamily: "Bebas Neue"),),
+                                          ],
+                                        ),
+                                      ),
+                                      GestureDetector(
+                                        onTap: () async {
+                                          showDialog(
+                                              context: context,
+                                              builder: (BuildContext context)=>LinkInfoWidget(linkId)
+                                          );
+                                          await Future.delayed(const Duration(milliseconds: 100));
+                                          overlayEntry.remove();
+                                        },
+                                        child: Container(
+                                          margin: const EdgeInsets.fromLTRB(24,0,24,19),
+                                          child: Row(
+                                            children: [
+                                              const Icon(Icons.share,color:kOrange,size: 30,),
+                                              const SizedBox(width: 10,),
+                                              Text("Referral link",style: kBodyText2.apply(fontFamily: "Bebas Neue"),),
+                                            ],
+                                          ),
+                                        ),
+                                      )
+                                    ],),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    );
+                    Overlay.of(context)?.insert(
+                        overlayEntry
+                    );
+
                   },
                   child: isUserDataLoaded
                       ? Consumer<User>(builder: (context, user, _) {
--- lib/widgets/info_dropdown.dart
+++ lib/widgets/info_dropdown.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:winhalla_app/config/themes/dark_theme.dart';
 
