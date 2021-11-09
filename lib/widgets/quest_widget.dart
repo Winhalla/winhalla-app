748f16a8057396147af10767de0e85dc27602640
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-11-09 17:56:46 +0100

    changed quests layout

--- lib/widgets/quest_widget.dart
+++ lib/widgets/quest_widget.dart
@@ -29,7 +29,7 @@ class QuestWidget extends StatelessWidget {
 
     return Container(
       decoration: BoxDecoration(color: kBackgroundVariant, borderRadius: BorderRadius.circular(16)),
-      padding: const EdgeInsets.fromLTRB(30, 27, 30, 27),
+      padding: const EdgeInsets.fromLTRB(30, 18, 20, 18),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
@@ -57,22 +57,52 @@ class QuestWidget extends StatelessWidget {
                 )
             ],
           ),
-          Stack(
-            children: [
-              SizedBox(
-                  width: 70,
-                  height: 70,
-                  child: CustomPaint(
-                    foregroundPainter: ProgressPainter(progressColor: color, percentage: percentage, width: 9),
-                  )),
-              Positioned.fill(
-                  child: Align(
-                      alignment: Alignment.center,
-                      child: Padding(
-                        padding: const EdgeInsets.only(top: 2), //add padding to center the font that has default bottom spacing
-                        child: Text("${isQuestFinished?100:percentage.ceil()}%", style: kBodyText4.apply(color: color)),
-                      ))),
-            ],
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+                color:kBlack
+            ),
+            padding: const EdgeInsets.fromLTRB(15, 15, 15, 10),
+            child: Column(
+              children: [
+                Stack(
+                  children: [
+                    SizedBox(
+                        width: 70,
+                        height: 70,
+                        child: CustomPaint(
+                          foregroundPainter: ProgressPainter(progressColor: color, percentage: percentage+.6, width: 9),
+                        ),
+                    ),
+                    Positioned.fill(
+                        child: Align(
+                            alignment: Alignment.center,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 2), //add padding to center the font that has default bottom spacing
+                              child: Text("${isQuestFinished?100:percentage.ceil()}%", style: kBodyText4.apply(color: color)),
+                            ))),
+                  ],
+                ),
+                const SizedBox(height: 15,),
+                Row(
+                  children: [
+                    Text(reward.toString() ,style: kBodyText4.apply(color:color),),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 1),
+                      child: Image.asset(
+                        "assets/images/coin.png",
+                        height: 20,
+                        width: 20,
+                        color: color,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           )
         ],
       ),
