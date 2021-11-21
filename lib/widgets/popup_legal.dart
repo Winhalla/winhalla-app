4486f819207519ebd95d293c220d0d0e9b19cc18
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-11-21 22:28:09 +0100

    made account deletion possible and finish logout function

--- lib/widgets/popup_legal.dart
+++ lib/widgets/popup_legal.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:winhalla_app/config/themes/dark_theme.dart';
+import 'package:winhalla_app/screens/login.dart';
+import 'package:winhalla_app/utils/get_uri.dart';
 import 'package:winhalla_app/utils/launch_url.dart';
+import 'package:winhalla_app/utils/services/secure_storage_service.dart';
 import 'package:winhalla_app/widgets/tip_painter.dart';
 import 'package:share/share.dart';
+import 'package:http/http.dart' as http;
 
 Widget LegalInfoPopup(){
-
+  bool confirmAccountDeletion = false;
   return StatefulBuilder(builder: (context, setState) {
     return AlertDialog(
       elevation: 10,
@@ -42,7 +46,7 @@ Widget LegalInfoPopup(){
       // contentPadding: const EdgeInsets.fromLTRB(24, 5, 24, 30),
       content: Padding(
         padding: const EdgeInsets.fromLTRB(4,0,4,0),
-        child: Column(
+        child: !confirmAccountDeletion?Column(
           mainAxisSize: MainAxisSize.min,
           children: [
             const Text("Privacy policy: ", style: kBodyText2,),
@@ -68,8 +72,44 @@ Widget LegalInfoPopup(){
               },
               child: Text("https://winhalla.app/legal", style: kBodyText3.apply(color: Colors.blueAccent),),
             ),
+            const SizedBox(height: 30,),
+            GestureDetector(
+              onTap: (){
+                setState((){
+                  confirmAccountDeletion = true;
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color:kBackground),
+                padding: const EdgeInsets.fromLTRB(22, 9, 22, 9),
+                child: Text("Delete account",style: kBodyText4.apply(color:kRed),),
+              ),
+            )
           ],
-        ),
+        ) : Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const Text("Are you sure you want to delete your account?",style: kBodyText2,),
+                const SizedBox(height: 7,),
+                Text("This action is not reversible",style: kBodyText3.apply(color:kText80),),
+                const SizedBox(height: 15,),
+                GestureDetector(
+                  onTap: () async {
+                    var status = await http.delete(getUri("/auth/deleteAccount"), headers: {"authorization": await getNonNullSSData("authKey")});
+                    if(status.statusCode >= 200 && status.statusCode <= 299){
+                      await secureStorage.write(key: "authKey", value: null);
+                      await GoogleSignInApi.logout();
+                      Navigator.pushReplacementNamed(context, "/login");
+                    }
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color:kRed),
+                    padding: const EdgeInsets.fromLTRB(22, 9, 22, 9),
+                    child: Text("Delete account",style: kBodyText4,),
+                  ),
+                )
+              ],
+        )
       ),
 
       actions: [
