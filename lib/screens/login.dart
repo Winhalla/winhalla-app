c8cd9a32d46ab7eb6bdc30e69fb06aed23279bd9
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2021-11-24 17:45:22 +0100

    simplified code

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,17 +1,9 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:winhalla_app/config/themes/dark_theme.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:winhalla_app/utils/get_uri.dart';
-import 'package:winhalla_app/utils/services/secure_storage_service.dart';
-import 'package:http/http.dart' as http;
 import 'package:winhalla_app/widgets/login/account_creation.dart';
-import 'dart:convert';
-
 import 'package:winhalla_app/widgets/login/enter_link.dart';
 import 'package:winhalla_app/widgets/login/google_apple_login.dart';
-
 
 class LoginPage extends StatelessWidget {
   final userData;
@@ -21,18 +13,16 @@ class LoginPage extends StatelessWidget {
   int step = 0;
   @override
   Widget build(BuildContext context) {
-
     List<Widget> screenList = [
-      // WinhallaPresentation(),
       const GoogleAppleLogin(),
       const EnterLink(),
       AccountCreation(accounts: accounts),
     ];
+
     try{
       if(userData?["data"]?["steam"] != null) step = 1;
     } catch(e) {}
-    if(accounts != null) step = 1;
-
+    if(accounts != null) step = 2;
 
     return SafeArea(
       child: Scaffold(
