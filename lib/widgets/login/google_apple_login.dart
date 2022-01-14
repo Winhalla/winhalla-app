699c04a19f1a459675bee0b18d2f725e1935c3d6
Philtrom <48993175+LoustuCraft@users.noreply.github.com>
2022-01-14 10:52:58 +0100

    added border to button

--- lib/widgets/login/google_apple_login.dart
+++ lib/widgets/login/google_apple_login.dart
@@ -171,9 +171,13 @@ class _GoogleAppleLoginState extends State<GoogleAppleLogin> {
                 child: Container(
                   decoration: BoxDecoration(
                     color: kBlack,
+                    border: Border.all(
+                      color: kPrimary,
+                      width: 1,
+                    ),
                     borderRadius: BorderRadius.circular(15),
                   ),
-                  padding: const EdgeInsets.fromLTRB(26.5, 20, 20, 20),
+                  padding: const EdgeInsets.fromLTRB(25.5, 19, 19, 19),
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
@@ -207,8 +211,12 @@ class _GoogleAppleLoginState extends State<GoogleAppleLogin> {
                   decoration: BoxDecoration(
                     color: kBlack,
                     borderRadius: BorderRadius.circular(15),
+                    border: Border.all(
+                      color: kPrimary,
+                      width: 1,
+                    ),
                   ),
-                  padding: const EdgeInsets.fromLTRB(25, 20, 20, 20),
+                  padding: const EdgeInsets.fromLTRB(24, 19, 19, 19),
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.center,
                     mainAxisSize: MainAxisSize.min,
